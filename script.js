e22e6183c2ea6065da5f78969c6c6bbb5c6fe1bb
purgz <henbrooks6@gmail.com>
2022-05-04 18:08:16 +0100

    More operators
    Added power, factorial, sqrt , ln, log, del buttons and functionality.

--- script.js
+++ script.js
@@ -18,27 +18,65 @@ function flipSign(num1){
     return num1 * -1;
 }
 
+function power(num1,num2){
+    return Math.pow(num1,num2);
+}
+
+function sqrt(num1){
+    return Math.sqrt(num1);
+}
+
+function factorial(num1){
+    let factorial = 1;
+    for (let i = 1; i <= num1; i ++){
+        factorial *= i;
+    }
+    return factorial;
+}
+
+function log(num1){
+    return Math.log(num1);
+}
+
+function logb10(num1){
+    return Math.log10(num1);
+}
+
 function operate(operator,num1,num2){
 
     num1 = Number(num1);
     num2 = Number(num2);
     
+    topDisplay.textContent = "";
+    topDisplay.textContent +=`${num1} ${operator} ${num2}`;
 
     switch (operator)
     {
         case "+":
-            return add(num1,num2);
-            
+            return add(num1,num2);  
         case "÷":
             return divide(num1,num2);
-            
         case "x":
             return multiply(num1,num2);
-            
         case "-":
             return subtract(num1,num2);
         case "±":
+            topDisplay.textContent = "";
             return flipSign(num1)
+        case "^":
+            return power(num1,num2);
+        case "!":
+            topDisplay.textContent = num1 + operator;
+            return factorial(num1);
+        case "√":
+            topDisplay.textContent = operator + num1;
+            return sqrt(num1);
+        case "ln":
+            topDisplay.textContent = `${operator}(${num1})`
+            return log(num1);
+        case "log":
+            topDisplay.textContent = `${operator}(${num1})`
+            return logb10(num1);
     }
 }
 var newValue = "";
@@ -47,10 +85,16 @@ var operator;
 
 const digitBtns = document.querySelectorAll(".digit");
 const currentDisplay = document.querySelector(".bottom");
+const topDisplay = document.querySelector(".top");
 const operatorBtns = document.querySelectorAll(".operator");
 const enterBtn = document.querySelector("#enter");
-const flipSignBtn = document.querySelector("#plus-minus");
+const singleOpBtns = document.querySelectorAll(".singleOperator");
+const deleteBtn = document.querySelector("#delete");
 
+deleteBtn.addEventListener("click",()=>{
+    previousValue = previousValue.toString().substring(0,previousValue.toString().length-1);
+    currentDisplay.textContent = previousValue;
+})
 
 digitBtns.forEach((button)=>{
     button.addEventListener("click",()=>{
@@ -65,6 +109,13 @@ digitBtns.forEach((button)=>{
     })
 })
 
+singleOpBtns.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        operator = button.textContent;
+        compute();
+    })
+})
+
 //set operator
 operatorBtns.forEach((button)=>{
     button.addEventListener("click",()=>{
@@ -75,18 +126,12 @@ operatorBtns.forEach((button)=>{
     })
 })
 
-
-flipSignBtn.addEventListener("click",(button)=>{
-    operator = flipSignBtn.textContent;
-    compute();
-})
-
-
 enterBtn.addEventListener("click",compute)
 
 //reset
 function clearDisplay(){
     currentDisplay.textContent = "";
+    topDisplay.textContent = "";
     previousValue = "";
     newValue = "";
     operator = null;
